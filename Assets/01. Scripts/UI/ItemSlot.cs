ea0119684d745e751122d0c36d8a21c126f0da68
LeeJongHyuk <jjong7477@gmail.com>
2025-05-23 14:40:22 +0900

    Feat : Eat Item

--- Assets/01. Scripts/UI/ItemSlot.cs
+++ Assets/01. Scripts/UI/ItemSlot.cs
@@ -1,15 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemSlot : MonoBehaviour
 {
+    public int quantity;
+    public Image icon;
+    public ItemData item;
     public GameObject selectedObject;
-
+    public TextMeshProUGUI quantityText;
+    
     private void Start()
     {
-        selectedObject = this.transform.Find("Selected").gameObject;
+        selectedObject = GameManager.FindChild(this.transform, "Selected").gameObject;
+        quantityText = GameManager.FindChild(this.transform, "QuantityText").GetComponent<TextMeshProUGUI>();
+        icon = GameManager.FindChild(this.transform, "Icon").GetComponent<Image>();
+        Clear();
+    }
+    
+    public void Set()
+    {
+        icon.gameObject.SetActive(true);
+        icon.sprite = item.icon;
+        quantityText.text = quantity > 1 ? quantity.ToString() : string.Empty;
+    }
+    
+    public void Clear()
+    {
+        item = null;
+        icon.gameObject.SetActive(false);
+        quantityText.text = string.Empty;
     }
 }
--- Assets/01. Scripts/UI/QuickSlotController.cs
+++ Assets/01. Scripts/UI/QuickSlotController.cs
@@ -10,8 +10,11 @@ public class QuickSlotController : MonoBehaviour
     public int currentIndex = 0;
     public int beforeIndex;
     public int maxIndex = 5;
+    private int _selectedItemIndex = 0;
     private float _scroll = 0f;
     private ItemSlot[] _itemSlots = new ItemSlot[5];
+    private ItemData _selectedItem;
+    private PlayerCondition _condition;
     
 
     private void Start()
@@ -20,6 +23,14 @@ public class QuickSlotController : MonoBehaviour
         {
             _itemSlots[i] = GameManager.FindChild(this.transform, $"ItemSlot{i}").GetComponent<ItemSlot>();
         }
+        
+        GameManager.Player.AddItem += AddItem;
+        _condition = GameManager.Player.Condition;
+    }
+
+    private void Update()
+    {
+        _selectedItem = _itemSlots[currentIndex].item;
     }
 
     public void OnScroll(InputAction.CallbackContext context)
@@ -27,7 +38,8 @@ public class QuickSlotController : MonoBehaviour
         Vector3 scroll = context.ReadValue<Vector2>();
         beforeIndex = currentIndex;
         _scroll += scroll.y;
-        Debug.Log(scroll.y);
+        
+        //Debug.Log(scroll.y);
         
         if (_scroll >= -1800f && _scroll < -1440f)
         {
@@ -50,11 +62,112 @@ public class QuickSlotController : MonoBehaviour
             currentIndex = 0;
         }
         
-
-        currentIndex = Mathf.Clamp(currentIndex, 0, maxIndex);
         _scroll = Mathf.Clamp(_scroll, -1800, 1);
-        
+
         _itemSlots[beforeIndex].selectedObject.SetActive(false);
         _itemSlots[currentIndex].selectedObject.SetActive(true);
+    }
+    
+    private void AddItem()
+    {
+        ItemData data = GameManager.Player.itemData;
+
+        if (data.canStack)
+        {
+            ItemSlot slot = GetItemStack(data);
+            if (slot != null)
+            {
+                slot.quantity++;
+                UIUpdate();
+                GameManager.Player.itemData = null;
+                return;
+            }
+        }
+
+        ItemSlot emptySlot = GetEmptySlot();
+
+        if (emptySlot != null)
+        {
+            emptySlot.item = data;
+            emptySlot.quantity = 1;
+            UIUpdate();
+            GameManager.Player.itemData = null;
+            return;
+        }
+
+        GameManager.Player.itemData = null;
+    }
+    
+    private ItemSlot GetItemStack(ItemData data)
+    {
+        for (int i = 0; i < _itemSlots.Length; i++)
+        {
+            if (_itemSlots[i].item == data && _itemSlots[i].quantity < data.maxStackAmount)
+            {
+                return _itemSlots[i];
+            }
+        }
+
+        return null;
+    }
+    
+    private void UIUpdate()
+    {
+        for (int i = 0; i < _itemSlots.Length; i++)
+        {
+            if (_itemSlots[i].item != null)
+            {
+                _itemSlots[i].Set();
+            }
+            else _itemSlots[i].Clear();
+        }
+    }
+    
+    private ItemSlot GetEmptySlot()
+    {
+        for (int i = 0; i < _itemSlots.Length; i++)
+        {
+            if (_itemSlots[i].item == null)
+            {
+                return _itemSlots[i];
+            }
+        }
+
+        return null;
+    }
+    
+    public void OnUseButton(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (_selectedItem.type == ItemType.Eatable)
+            {
+                for (int i = 0; i < _selectedItem.eatables.Length; i++)
+                {
+                    switch (_selectedItem.eatables[i].type)
+                    {
+                        case EatableType.Health:
+                            _condition.Heal(_selectedItem.eatables[i].value);
+                            break;
+                    }
+                }
+
+                RemoveSelectedItem();
+            }
+        }
+    }
+    
+    private void RemoveSelectedItem()
+    {
+        _itemSlots[currentIndex].quantity--;
+
+        if (_itemSlots[currentIndex].quantity <= 0)
+        {
+            _selectedItem = null;
+            _itemSlots[currentIndex].item = null;
+            _selectedItemIndex = -1;
+        }
+        
+        UIUpdate();
     }
 }
